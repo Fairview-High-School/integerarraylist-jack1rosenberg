435ec0f14d9c5aa90855c10c3c2f56a5cfe8d87c
Jack <jackenberg99@gmail.com>
2024-09-10 21:54:43 +0000

    initial

--- IntegerArrayList.java
+++ IntegerArrayList.java
@@ -1,2 +1,34 @@
-public class IntegerArrayList extends IntegerList{
+public class IntegerArrayList implements IntegerList{
+    private int[] data;
+    private int size; 
+
+    public IntegerArrayList()
+    {
+        data = new int[10];
+        size = 0;
+    }
+
+    public void add(int val){
+
+    }
+
+    public void add(int index, int val)
+    {
+
+    }
+
+    public void set(int index, int val)
+    {
+
+    }
+
+    public void clear()
+    {
+
+    }
+
+    public E remove(int index)
+    {
+        
+    }
 }
